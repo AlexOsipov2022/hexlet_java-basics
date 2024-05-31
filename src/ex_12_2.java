aa018140f7767260b99fa83475b3cd71f92eeb0c
AlexOsipov <osipovalex2022@gmail.com>
2024-05-31 13:02:32 +0300

    ex_13 modify

---
+++ src/ex_12_2.java
@@ -0,0 +1,5 @@
+public class ex_12_2 {
+    public static void main(String[] args) {
+        ex_12.printCapitalize(args);
+    }
+}
--- src/ex_13.java
+++ src/ex_13.java
@@ -1,6 +1,6 @@
 public class ex_13 {
-    public static void printRandomNumber(String[] args) {
-        System.out.println((int) Math.random() * 10);
+    public static void printRandomNumber() {
+        System.out.println((int) (Math.random() * 100));
     }
 }
 
