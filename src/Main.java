03b55130efb91ab30873e959d7144bd83ad1cfe8
AlexOsipov <osipovalex2022@gmail.com>
2024-05-31 12:16:12 +0300

    ex_13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+       public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+           System.out.print("Введите строку: ");
+       String res = scan.nextLine();
+       System.out.println(res.toUpperCase().charAt(0) + res.toLowerCase().substring(1));
     }
 }
+
--- src/ex_12.java
+++ src/ex_12.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class ex_12 {
         public static void printCapitalize(String[] args) {
-        var name = "heXlEt";
-        System.out.println(name.toUpperCase().charAt(0) + name.toLowerCase().substring(1));
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Введите строку: ");
+            String res = scan.nextLine();
+            System.out.println(res.toUpperCase().charAt(0) + res.toLowerCase().substring(1));
 
     }
 }
