b07b07d1b5f99dea473ac9c938b5b141c96513e1
AlexOsipov <osipovalex2022@gmail.com>
2024-05-31 13:13:52 +0300

    ex_14

---
+++ src/ex_14.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ex_14 {
+//Рассчитаем количество дней между двумя датами, используя встроенные возможности Java.
+public static void main(String[] args) {
+            // С даты
+    LocalDate dateFrom = LocalDate.of(2024, Month.MAY, 31);
+// По дату
+    LocalDate dateTo = LocalDate.of(2024, Month.JULY, 29);
+// Количество дней между этими датами
+    long noOfDaysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
+    System.out.println(noOfDaysBetween);
+        }
+    }
