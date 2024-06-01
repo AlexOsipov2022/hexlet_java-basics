fe4c0d7266ae8d9e6ddc4850dc9ec0a357f84efb
AlexOsipov <osipovalex2022@gmail.com>
2024-06-01 11:35:16 +0300

    ex_14

--- src/ex_20.java
+++ src/ex_20.java
@@ -3,6 +3,7 @@ public class ex_20 {
         return "*".repeat(x) + number.substring(12);
     }
     public static String getHiddenCard(String number) {
-        return "*".repeat(4) + number.substring(12);
+       // return "*".repeat(4) + number.substring(12);
+       return ex_20.getHiddenCard(number, 4);
     }
     }
