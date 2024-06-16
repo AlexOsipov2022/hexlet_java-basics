54624df14c4689e8748d27d9f18800dc3d3ab8f0
AlexOsipov <osipovalex2022@gmail.com>
2024-06-16 14:54:42 +0300

    ex_14

---
+++ src/ex_28.java
@@ -0,0 +1,10 @@
+public class ex_28 {
+    public static String encrypt(String str) {
+             var result = "";
+            for (var i = 0; i < str.length(); i += 2) {
+                var nextSymbol = (i + 2 > str.length()) ? "" : str.charAt(i + 1);
+                result = result + nextSymbol + str.charAt(i);
+        }
+        return result;
+    }
+}
