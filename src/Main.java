b19d84334506969ccc4c26c0aecf3294675fa386
AlexOsipov <osipovalex2022@gmail.com>
2024-06-16 13:46:51 +0300

    ex_14

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@
 
 public class Main {
        public static void main(String[] args) {
-          //ex_12.printCapitalize(args);
-          //ex_13.printRandomNumber();
-          //ex_14.countday();
-          //ex_20.getHiddenCard("1234567812345678", 3);
-         //ex_22.isLeapYear(2016);
-        // ex_23.convertString("");
-       //ex_24.getNumberExplanation(42);
-      // ex_25.printNumbers(0);
-
+           //ex_12.printCapitalize(args);
+           // ex_13.printRandomNumber();
+           // ex_14.countday();
+           // ex_20.getHiddenCard("1234567812345678", 3);
+           //ex_22.isLeapYear(2016);
+           // ex_23.convertString("");
+           // ex_24.getNumberExplanation(42);
+           // ex_25.printNumbers(0);
                  }
 }
 
